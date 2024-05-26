355f7093417a2a9bfb6c64b61501db2fe0c944cc
SFukayama <syoqwert@gmail.com>
2024-05-26 14:03:57 +0900

    書籍個別の管理画面実装

--- OnlineBookStore/src/main/java/com/example/OnlineBookStore/controller/management/BookManagementController.java
+++ OnlineBookStore/src/main/java/com/example/OnlineBookStore/controller/management/BookManagementController.java
@@ -3,20 +3,21 @@ package com.example.OnlineBookStore.controller.management;
 import com.example.OnlineBookStore.application.service.BookApplicationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("management")
 @RequiredArgsConstructor
-public class ManagementTopController {
+public class BookManagementController {
 
     private final BookApplicationService bookApplicationService;
-    @GetMapping("/top")
-    public String managementTop(Model model){
-        model.addAttribute("bookList", bookApplicationService.findAll());
-        return "management/top";
+
+    @GetMapping("/{bookId}")
+    public String bookManagement(@PathVariable Integer bookId, Model model) {
+        model.addAttribute("bookDetail", bookApplicationService.findBookDetailById(bookId));
+        return "management/book";
     }
 }
