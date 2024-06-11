0f596c5c931bf1676ddb88a6395e0618f841b01a
SFukayama <syoqwert@gmail.com>
2024-06-11 11:43:33 +0900

    ディレクトリ構造変更 　- cart.htmlを移動

--- OnlineBookStore/src/main/java/com/example/OnlineBookStore/controller/cart/CartIndexController.java
+++ OnlineBookStore/src/main/java/com/example/OnlineBookStore/controller/cart/CartIndexController.java
@@ -5,17 +5,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/cart")
 public class CartIndexController {
 
     private final CartApplicationService cartApplicationService;
 
-    @GetMapping("/cart")
+    @GetMapping
     public String showCartByCustomerId(Model model) {
         int customerId = 1; // TODO カスタマーIDの管理・受け渡しについて実装
         model.addAttribute("bookListInCart", cartApplicationService.showCartByCustomerId(customerId));
-        return "book/cart";
+        return "cart/cart";
     }
 }
--- OnlineBookStore/src/main/java/com/example/OnlineBookStore/controller/cart/CartItemController.java
+++ OnlineBookStore/src/main/java/com/example/OnlineBookStore/controller/cart/CartItemController.java
@@ -4,15 +4,18 @@ import com.example.OnlineBookStore.application.service.CartApplicationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/cart")
+
 public class CartItemController {
 
     private final CartApplicationService cartApplicationService;
 
-    @PostMapping("/cart/delete")
+    @PostMapping("/delete")
     public String deleteItemInCart(@RequestParam("bookId") Integer bookId) {
         int customerId = 1; // TODO カスタマーID
         cartApplicationService.deleteItemInCart(customerId, bookId);
