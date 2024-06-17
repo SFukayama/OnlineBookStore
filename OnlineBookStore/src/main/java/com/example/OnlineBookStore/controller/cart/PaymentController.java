faf6e1ea62e4a285e88e37e24d3e3d238a28a544
SFukayama <syoqwert@gmail.com>
2024-06-17 11:24:50 +0900

    決済画面を実装　WIP  - 画面とコントローラ(READ)のみ。

---
+++ OnlineBookStore/src/main/java/com/example/OnlineBookStore/controller/cart/PaymentController.java
@@ -0,0 +1,31 @@
+package com.example.OnlineBookStore.controller.cart;
+
+import com.example.OnlineBookStore.application.service.CartApplicationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/cart/payment")
+public class PaymentController {
+
+    private final CartApplicationService cartApplicationService;
+
+    @GetMapping
+    public String showCartByUsername(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String username = userDetails.getUsername();
+        model.addAttribute("bookListInCart", cartApplicationService.showCartByUsername(username));
+        model.addAttribute("totalPrice", cartApplicationService.calculateTotalPriceInCart(username));
+        return "cart/payment";
+    }
+
+
+}
