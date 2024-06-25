92084946713248b20dbdc4dcf40751050d8fe1eb
SFukayama <syoqwert@gmail.com>
2024-06-25 11:50:39 +0900

    Formクラスにアノテーション付与 AdminRegistrationFormを、管理者登録画面に文言変更

--- OnlineBookStore/src/main/java/com/example/OnlineBookStore/controller/management/AdminRegistrationForm.java
+++ OnlineBookStore/src/main/java/com/example/OnlineBookStore/controller/management/AdminRegistrationForm.java
@@ -1,12 +1,17 @@
 package com.example.OnlineBookStore.controller.management;
 
 import com.example.OnlineBookStore.domain.model.User;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class AdminRegistrationForm {
 
+    @NotBlank
     private String username;
+
+    @NotBlank
     private String password;
+
     private User.Authority authority;
 }
--- OnlineBookStore/src/main/java/com/example/OnlineBookStore/controller/user/UserRegistrationForm.java
+++ OnlineBookStore/src/main/java/com/example/OnlineBookStore/controller/user/UserRegistrationForm.java
@@ -1,12 +1,17 @@
 package com.example.OnlineBookStore.controller.user;
 
 import com.example.OnlineBookStore.domain.model.User;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class UserRegistrationForm {
 
+    @NotBlank
     private String username;
+
+    @NotBlank
     private String password;
+
     private User.Authority authority;
 }
